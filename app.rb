b7fb0ba6356e5178fd3408fdc5d1e8da40b94d13
Antony Sanders <antony.devon92@gmail.com>
2020-02-24 18:43:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "my_session_secret"
+  end
+  
+  get '/' do 
+    erb :index
+  end 
+  
+  get '/checkout' do
+    session["item"] = "#{params[:]}"
+    @session = session
+    
+    erb :checkout
+  end 
+  
+  
 end
